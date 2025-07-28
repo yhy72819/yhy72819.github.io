0e0b95687716e407ef631af155f84447f2482212
yhy72819 <yuanhuayun713@gmail.com>
2025-07-28 22:05:11 +0800

    更换作者头像

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -29,7 +29,9 @@ export default hopeTheme({
 
   // 博客相关
   blog: {
+    name: "yhy72819",
     description: "一个前端开发者",
+    avatar: "/photos/avatar.jpg",
     intro: "/intro.html",
     medias: {
       Baidu: "https://example.com",
