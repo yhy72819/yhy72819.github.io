3f78761374a7b525e73c821bc707dbfed6a89044
yhy72819 <yuanhuayun713@gmail.com>
2025-08-05 21:24:37 +0800

    搜索

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -137,6 +137,15 @@ export default hopeTheme({
   // 在这里配置主题提供的插件
   plugins: {
     blog: true,
+    search: {
+      maxSuggestions: 32,
+      hotKeys: [],
+      locales: {
+        "/": {
+          placeholder: "搜索"
+        }
+      }
+    },
 
     // 启用之前需安装 @waline/client
     // 警告: 这是一个仅供演示的测试服务，在生产环境中请自行部署并使用自己的服务！
